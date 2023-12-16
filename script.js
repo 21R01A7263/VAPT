1fee3c42016944582cbeda280b21d62a95ba1ca3
Gaurav Sai Teja <128503039+21R01A7263@users.noreply.github.com>
2023-12-16 20:49:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { signIn } from "./utils.js";
 
-let CLIENT_ID ="864591496189-f8fqrsai6d9td034r3nfs1vcg0e0fiff.apps.googleusercontent.com";
-let REDIRECT_URI = "http://127.0.0.1:5501/profile.html";
+let CLIENT_ID ="508774011704-rv5jvasfam9rkp7angkn3fbs6u62vi1e.apps.googleusercontent.com";
+let REDIRECT_URI = "https://vapt.vercel.app/profile.html";
 let SCOPES = "https://www.googleapis.com/auth/drive";
 
 let button = document.getElementById("signin");
